932f09ff63326ca9857c76ef89cfa6f3d9569e64
powerb <powerb@kamis-macbook-pro.local>
2019-04-02 13:22:29 -0600

    test commit

---
+++ Git01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git01
+//
+//  Created by powerb on 4/2/19.
+//  Copyright © 2019 powerb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git01
+//
+//  Created by powerb on 4/2/19.
+//  Copyright © 2019 powerb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
